c12602833d87d0a411cb93fc148baed843bfa299
spariyangat <spariyangat05@gmail.com>
2025-07-09 22:15:52 -0700

    Updated LED/button logic

--- simple_led_on.py
+++ simple_led_on.py
@@ -1,15 +1,28 @@
 import RPi.GPIO as GPIO
 import time
 
+print("Script started!")
 GPIO.setmode(GPIO.BCM)
+
 LED_PIN = 18
+BUTTON_PIN = 4
 
 GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(BUTTON_PIN, GPIO.IN)
 
-GPIO.output(LED_PIN, GPIO.HIGH)
+GPIO.output(LED_PIN, GPIO.LOW) #making sure the LED starts off
 
-time.sleep(10)
 
-GPIO.output(LED_PIN, GPIO.LOW)
+
+try: 
+    while True:
+        if(GPIO.input(BUTTON_PIN) == GPIO.HIGH):
+            GPIO.output(LED_PIN, GPIO.HIGH)
+        else:
+            GPIO.output(LED_PIN, GPIO.LOW)pyho
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
 
 GPIO.cleanup()
